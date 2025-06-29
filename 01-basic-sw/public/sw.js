d18b0dc78e1aa5ab626369d25beab76d356e3256
adhitamafikri <adhitamafikri.dev@gmail.com>
2025-06-29 23:30:08 +0700

    feat(01-basic-sw): can now write Service Worker in TypeScript

--- 01-basic-sw/public/sw.js
+++ 01-basic-sw/public/sw.js
@@ -1,22 +1,25 @@
 self.addEventListener("install", (event) => {
-  console.log("V1 installing…");
-
-  // cache a mouse SVG
-  event.waitUntil(
-    caches.open("static-v1").then((cache) => cache.add("/mouse.svg"))
-  );
+    console.log("SW V1 installing…");
+    // cache a mouse SVG
+    event.waitUntil(caches.open("static-v1").then((cache) => cache.add("/mouse.svg")));
 });
-
 self.addEventListener("activate", (event) => {
-  console.log("V1 now ready to handle fetches!");
+    console.log("V1 now ready to handle fetches guys!");
 });
-
 self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-
-  // serve the sheep SVG from the cache if the request is
-  // same-origin and the path is '/mouse.svg'
-  if (url.origin == location.origin && url.pathname == "/sheep.svg") {
-    event.respondWith(caches.match("/mouse.svg"));
-  }
+    const url = new URL(event.request.url);
+    // serve the sheep SVG from the cache if the request is
+    // same-origin and the path is '/mouse.svg'
+    if (url.origin == location.origin && url.pathname == "/sheep.svg") {
+        // event.respondWith(caches.match("/mouse.svg"));
+        caches.match("/mouse.svg").then((response) => {
+            if (response) {
+                console.log("Found response in cache for this resource:", url.href);
+                event.respondWith(response);
+            }
+            else {
+                console.log("No response found in cache for this resource:", url.href);
+            }
+        });
+    }
 });
---
+++ 01-basic-sw/src/sw/sw.ts
@@ -0,0 +1,30 @@
+self.addEventListener("install", (event: ExtendableEvent) => {
+  console.log("SW V1 installing…");
+
+  // cache a mouse SVG
+  event.waitUntil(
+    caches.open("static-v1").then((cache) => cache.add("/mouse.svg"))
+  );
+});
+
+self.addEventListener("activate", (event: ExtendableEvent) => {
+  console.log("V1 now ready to handle fetches guys!");
+});
+
+self.addEventListener("fetch", (event: FetchEvent) => {
+  const url = new URL(event.request.url);
+
+  // serve the sheep SVG from the cache if the request is
+  // same-origin and the path is '/mouse.svg'
+  if (url.origin == location.origin && url.pathname == "/sheep.svg") {
+    // event.respondWith(caches.match("/mouse.svg"));
+    caches.match("/mouse.svg").then((response) => {
+      if (response) {
+        console.log("Found response in cache for this resource:", url.href);
+        event.respondWith(response);
+      } else {
+        console.log("No response found in cache for this resource:", url.href);
+      }
+    });
+  }
+});
