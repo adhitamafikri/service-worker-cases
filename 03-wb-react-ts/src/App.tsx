fb938cd74a533f51ea4e0820bf1a3334a850cacc
adhitamafikri <adhitamafikri.dev@gmail.com>
2025-07-10 20:42:24 +0700

    feat(03-wb-react-ts): can now build the sw properl, register and control the app

--- 03-wb-react-ts/src/App.tsx
+++ 03-wb-react-ts/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="sw-img-group">
+        <div id="sw-img-1" className="sw-img"></div>
+        <div id="sw-img-2" className="sw-img"></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03-wb-react-ts/src/sw/sw.ts
+++ 03-wb-react-ts/src/sw/sw.ts
@@ -1,18 +1,70 @@
-const expectedCaches = ["static-v2"];
+import { registerRoute, Route } from "workbox-routing";
+import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
 
+const SW_VERSION = "v1.0.0";
+const cacheNames = {
+  images: `images-${SW_VERSION}`,
+  scripts: `scripts-${SW_VERSION}`,
+  styles: `styles-${SW_VERSION}`,
+};
+
+// Define route for caching images
+// const imageRoute = new Route(
+//   ({ request }) => request.destination === "image",
+//   new NetworkFirst({
+//     cacheName: cacheNames.images,
+//     plugins: [
+//       new ExpirationPlugin({
+//         maxAgeSeconds: 60 * 5, // 5 minutes
+//       }),
+//     ],
+//   })
+// );
+
+// Define route for caching scripts
+const scriptsRoute = new Route(
+  ({ request }) => request.destination === "script",
+  new NetworkFirst({
+    cacheName: cacheNames.scripts,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 50, // Limit to 50 scripts
+        maxAgeSeconds: 60 * 60 * 24, // 1 day
+      }),
+    ],
+  })
+);
+
+// Define route for caching styles
+const stylesRoute = new Route(
+  ({ request }) => request.destination === "style",
+  new NetworkFirst({
+    cacheName: cacheNames.styles,
+  })
+);
+
+// register the routes
+// These routes will handle requests for images, scripts, and styles
+// registerRoute(imageRoute);
+registerRoute(scriptsRoute);
+registerRoute(stylesRoute);
+
+// Precache particular resources during the install phase
 async function precacheResources() {
   try {
-    const cache = await caches.open("static-v2");
-    // cache.addAll(["/mouse.svg"]);
+    const cache = await caches.open(cacheNames.images);
     cache.addAll(["/camel.svg", "/elephant.svg"]);
   } catch (error) {
     console.error("Failed to precache resources:", error);
   }
 }
 
+// We want to clear any caches that were used by previous versions of the Service Worker
 async function clearUnusedCaches() {
   try {
     const cacheKeys = await caches.keys();
+    const expectedCaches = Object.values(cacheNames);
     Promise.all(
       cacheKeys.map((key) => {
         if (!expectedCaches.includes(key)) {
@@ -40,8 +92,9 @@ self.addEventListener("activate", (event: ExtendableEvent) => {
 });
 
 self.addEventListener("fetch", async (event: FetchEvent) => {
-  console.log("from the service worker fetch event: ", event);
+  console.log("from the service worker fetch event: ", event.request.url);
   const url = new URL(event.request.url);
+  console.log("from the service worker fetch event new URL(): ", url.pathname);
 
   if (url.origin == location.origin) {
     if (url.pathname == "/sheep.svg") {
@@ -50,5 +103,11 @@ self.addEventListener("fetch", async (event: FetchEvent) => {
     if (url.pathname == "/mouse.svg") {
       event.respondWith(caches.match("/elephant.svg"));
     }
+  }
+});
+
+self.addEventListener("message", (event: ExtendableMessageEvent) => {
+  if (event.data.type === "GET_VERSION") {
+    event.ports[0].postMessage(SW_VERSION);
   }
 });
